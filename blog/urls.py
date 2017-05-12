e1bd85bb65f1a6534674831c244852712c55520d
= <=>
2017-05-12 10:40:10 -0400

    New Templates

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url, include
-from .views import index
+from .views import post_list, post_new, post_edit, post_detail
 
 
 urlpatterns = [
-    url(r'^$', index, name = 'index'),
+    url(r'^$', post_list, name = 'post_list'),
+    url(r'^post/(?P<pk>[0-9]+)/$', post_detail, name='post_detail'),
+    url(r'^post/new/$', post_new, name='post_new'),
+    # url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', post_edit, name='post_edit'),
 ]
